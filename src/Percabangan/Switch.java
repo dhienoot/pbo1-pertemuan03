e3f90a14be773cd16059b5a2eda5ec586fe12906
muhammad <dinoganteng7700@gmail.com>
2020-04-12 13:54:31 +0800

    Percabangan Switch

---
+++ src/Percabangan/Switch.java
@@ -0,0 +1,25 @@
+package Percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int JalanJalan=3;
+        switch(JalanJalan){
+            case 1:
+                System.out.println("orang gila");
+                break;
+            case 2:
+                System.out.println("orang kesambet");
+                break;
+            case 3:
+                System.out.println("vave sagan ubat nyamuk");
+                break;
+            case 4:
+                System.out.println("urang lamak");
+                break;
+            default:
+                System.out.println("urang halus");
+                break;
+
+        }
+    }
+}
