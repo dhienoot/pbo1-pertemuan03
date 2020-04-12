2098dfcdb4f2ea625c27fdf49e6469fca48d7a20
muhammad <dinoganteng7700@gmail.com>
2020-04-12 12:36:05 +0800

    Buat Class OperasiString

---
+++ src/String/OperasiString.java
@@ -0,0 +1,9 @@
+package String;
+
+public class OperasiString {
+    public static void main(String[] args) {
+        String kota = "Kalimantan Selatan";
+
+        System.out.println(kota);
+    }
+}
