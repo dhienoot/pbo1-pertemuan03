5204fd15193544ef1c1c0e287a68448a1c4aafeb
muhammad <dinoganteng7700@gmail.com>
2020-04-12 13:39:42 +0800

    Uang tidak cukup

--- src/Percabangan/IfElse.java
+++ src/Percabangan/IfElse.java
@@ -2,8 +2,8 @@ package Percabangan;
 
 public class IfElse {
     public static void main(String[] args) {
-        double totalBelanja = 80000;
-        double uangDiDompet = 100000;
+        double totalBelanja = 120000;
+        double uangDiDompet = 80000;
 
         if(uangDiDompet<totalBelanja){
             System.out.println("uang kamu kurang, belanja yang penting saja");
